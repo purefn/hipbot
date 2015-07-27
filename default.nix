29ba626a79a2032cbc03b1fd5ccbf73bb65d19f4
Richard Wallace <rwallace@thewallacepack.net>
2015-07-26 20:25:55 -0700

    update travis config to use containers

--- default.nix
+++ default.nix
@@ -1,20 +1,3 @@
-{ mkDerivation, aeson, base, bifunctors, blaze-builder, bytestring
-, either, exceptions, http-client, http-client-tls, http-types, jwt
-, lens, mtl, network-uri, postgresql-simple, resource-pool, safe
-, stdenv, stm, text, time, transformers, unordered-containers
-, utf8-string, wai, wai-lens, webcrank-wai, wreq
-}:
-mkDerivation {
-  pname = "hipbot";
-  version = "0.6";
-  src = builtins.filterSource (path: type: baseNameOf path != ".git" && baseNameOf path != "dist") ./.;
-  buildDepends = [
-    aeson base bifunctors blaze-builder bytestring either exceptions
-    http-client http-client-tls http-types jwt lens mtl network-uri
-    postgresql-simple resource-pool safe stm text time transformers
-    unordered-containers utf8-string wai wai-lens webcrank-wai wreq
-  ];
-  homepage = "https://github.com/purefn/hipbot";
-  description = "A library for building HipChat Bots";
-  license = stdenv.lib.licenses.bsd3;
-}
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7101" }:
+
+nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./hipbot.nix { }
---
+++ hipbot.nix
@@ -0,0 +1,21 @@
+{ mkDerivation, aeson, base, bifunctors, blaze-builder, bytestring
+, either, exceptions, http-client, http-client-tls, http-types, jwt
+, lens, mtl, network-uri, postgresql-simple, resource-pool, safe
+, stdenv, stm, text, time, transformers, unordered-containers
+, utf8-string, wai, wai-lens, webcrank, webcrank-wai, wreq
+}:
+mkDerivation {
+  pname = "hipbot";
+  version = "0.6";
+  src = builtins.filterSource (path: type: baseNameOf path != ".git" && baseNameOf path != "dist") ./.;
+  buildDepends = [
+    aeson base bifunctors blaze-builder bytestring either exceptions
+    http-client http-client-tls http-types jwt lens mtl network-uri
+    postgresql-simple resource-pool safe stm text time transformers
+    unordered-containers utf8-string wai wai-lens webcrank webcrank-wai
+    wreq
+  ];
+  homepage = "https://github.com/purefn/hipbot";
+  description = "A library for building HipChat Bots";
+  license = stdenv.lib.licenses.bsd3;
+}
